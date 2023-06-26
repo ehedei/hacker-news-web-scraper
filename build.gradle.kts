064587079307da3407764558dc19071c8cddeb3c
Ehedei <ehedeidev@gmail.com>
2023-06-26 18:39:28 +0100

    Gradle configuration changed to build fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    application
+    kotlin("jvm") version "1.6.0"
 }
 
 group = "web.scraping.hacker.news"
@@ -14,6 +16,27 @@ dependencies {
     implementation("org.apache.commons:commons-lang3:3.12.0")
     testImplementation(platform("org.junit:junit-bom:5.9.2"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+}
+
+application {
+    mainClass.set("web.scraping.hacker.news.Main")
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        archiveClassifier.set("standalone")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+                .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar)
+    }
 }
 
 tasks.test {
