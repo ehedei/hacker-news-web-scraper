ac9517740cb07aed68dd83251ffbf6f9637554b1
Ehedei <ehedeidev@gmail.com>
2023-06-26 17:00:12 +0100

    Application entry point added

--- src/main/java/web/scraping/hacker/news/Main.java
+++ src/main/java/web/scraping/hacker/news/Main.java
@@ -1,19 +1,45 @@
 package web.scraping.hacker.news;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import web.scraping.hacker.news.converters.HtmlNewsArticleConverter;
+import web.scraping.hacker.news.converters.imp.HtmlNewsArticleConverterImp;
+import web.scraping.hacker.news.domain.NewsArticle;
+import web.scraping.hacker.news.services.NewsService;
+import web.scraping.hacker.news.services.ScrapingService;
+import web.scraping.hacker.news.services.imp.NewsServiceImp;
+import web.scraping.hacker.news.services.imp.ScrapingServiceImp;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
+
+    private static final String DEFAULT_API_URL = "https://news.ycombinator.com/";
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String apiUrl = DEFAULT_API_URL;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        if (args.length > 0) {
+            apiUrl = args[0];
+        }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        final HtmlNewsArticleConverter htmlNewsArticleConverter = new HtmlNewsArticleConverterImp();
+        final ScrapingService scrapingService = new ScrapingServiceImp(htmlNewsArticleConverter);
+        final NewsService newsService = new NewsServiceImp();
+
+        try {
+            final List<NewsArticle> newsArticles = scrapingService.getNewsFromUrl(apiUrl);
+            System.out.println("Scraped News:\n");
+            newsArticles.forEach(System.out::println);
+
+            final List<NewsArticle> longTitleArticles = newsService.filterNewsByLongTitlesSortedByComments(newsArticles);
+            System.out.println("\n\nAll previous entries with more than five words in the title ordered by the number of comments first:\n");
+            longTitleArticles.forEach(System.out::println);
+
+            final List<NewsArticle> shortTitleArticles = newsService.filterNewsByShortTitlesSortedByPoints(newsArticles);
+            System.out.println("\n\nAll previous entries with less than or equal to five words in the title ordered by points:\n");
+            shortTitleArticles.forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
     }
 }
